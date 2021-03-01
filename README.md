bc746ef7f56b2904280b9bbe895c0e539451cbc5
Altay Simsek <altaysimsek16@gmail.com>
2021-03-01 13:10:07 +0300

    Create readme.md && lint && prettier

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Insdl 
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## 🚀 How it work
+Insdl is a chrome extension for downloading posts video. The extension works as a content script so only reads information from the DOM and generates a button for download.
+
+
+## Screenshots
+![How it is work](./assets/anImage.png)
+
--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,59 +1,57 @@
 $(function () {
-    console.log('%c[V1.0 - Insdl] Insdl is ready!', 'color: aqua; font-weight: 700');
+  console.log('%c[V1.0 - Insdl] Insdl is ready!', 'color: aqua; font-weight: 700')
 
-    var getVideoUrl = () => {
-        setTimeout(() => {
-            var scripts = $('script');
-            var additionalData = null;
-            for (var i = 0; i < scripts.length; i++) {
-                var scrInner = scripts[i].innerHTML;
-                if (scrInner.startsWith('window.__additionalDataLoaded')) {
-                    additionalData = scripts[i].innerHTML;
-                }
-            }
-            // Worked Case : https://www.instagram.com/p/CL1Rv-Ylrkc/
-            // console.log(additionalData);
+  const getVideoUrl = () => {
+    setTimeout(() => {
+      const scripts = $('script')
+      let additionalData = null
+      for (let i = 0; i < scripts.length; i++) {
+        const scrInner = scripts[i].innerHTML
+        if (scrInner.startsWith('window.__additionalDataLoaded')) {
+          additionalData = scripts[i].innerHTML
+        }
+      }
+      // Worked Case : https://www.instagram.com/p/CL1Rv-Ylrkc/
+      // console.log(additionalData);
 
-            var dataToJson = JSON.parse(
-                additionalData.slice(
-                    31 + window.location.pathname.length + 2,
-                    additionalData.length - 2
-                )
-            );
-            // Kaydırmalılar için Case1- dataToJson.graphql.shortcode_media.edge_sidecar_to_children.edges[0].node.video_url
-            // Tek video için Case2- dataToJson.graphql.shortcode_media.video_url
-            //Kaydırmalılarda hata veriyor.
-            if (dataToJson.graphql.shortcode_media.video_url) {
-                $('._5e4p').after(
-                    `<a class="downButton" href="${dataToJson.graphql.shortcode_media.video_url}&dl=1" target="_blank" download="Insdl">Insdl</a>`
-                );
-                console.log(dataToJson.graphql.shortcode_media.video_url);
-            } else {
-                console.log('Video bulunamadı');
-            }
-        }, 1000);
-    };
-    getVideoUrl();
-    var windowLoc = window.location.href;
+      const dataToJson = JSON.parse(
+        additionalData.slice(31 + window.location.pathname.length + 2, additionalData.length - 2)
+      )
+      // Kaydırmalılar için Case1- dataToJson.graphql.shortcode_media.edge_sidecar_to_children.edges[0].node.video_url
+      // Tek video için Case2- dataToJson.graphql.shortcode_media.video_url
+      // Kaydırmalılarda hata veriyor.
+      if (dataToJson.graphql.shortcode_media.video_url) {
+        $('._5e4p').after(
+          `<a class="downButton" href="${dataToJson.graphql.shortcode_media.video_url}&dl=1" target="_blank" download="Insdl">Insdl</a>`
+        )
+        console.log(dataToJson.graphql.shortcode_media.video_url)
+      } else {
+        console.log('Video bulunamadı')
+      }
+    }, 1000)
+  }
+  getVideoUrl()
 
-    var observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (window.location.href != windowLoc) {
-                console.log('%c[V1.0 - Insdl] Page changed!', 'color: red; font-weight: 600');
-                windowLoc = window.location.href;
-                if (window.location.href.startsWith('https://www.instagram.com/p/')) {
-                    location.reload();
-                    getVideoUrl();
-                }
-            }
-        });
-    });
+  let windowLoc = window.location.href
 
-    var config = {
-        childList: true,
-        subtree: true,
-        attributes: true,
-    };
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (window.location.href !== windowLoc) {
+        console.log('%c[V1.0 - Insdl] Page changed!', 'color: red; font-weight: 600')
+        windowLoc = window.location.href
+        if (window.location.href.startsWith('https://www.instagram.com/p/')) {
+          location.reload()
+          getVideoUrl()
+        }
+      }
+    })
+  })
 
-    observer.observe(document, config);
-});
+  const config = {
+    childList: true,
+    subtree: true,
+    attributes: true
+  }
+
+  observer.observe(document, config)
+})
